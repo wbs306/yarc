38397a7ef532ef072b68a6eb2311eeedbb2df87b
wbs306 <wbs306@gmail.com>
2026-07-21 00:44:15 +0800

    fix: hide library shortcut in journal browser

--- apps/web/src/pages/PapersPage.vue
+++ apps/web/src/pages/PapersPage.vue
@@ -3026,7 +3026,7 @@ const showSearchPaperPopup = (paper: any) => {
           </Teleport>
 
           <div class="side-bottom">
-            <button v-if="sidebarMode !== 'library'" class="settings-link" @click="setSidebarMode('library')">
+            <button v-if="sidebarMode !== 'library' && sidebarMode !== 'ieee'" class="settings-link" @click="setSidebarMode('library')">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/></svg>
               <span>文献库</span>
             </button>
